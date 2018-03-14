1ad25dcf88cdab1de5c27ebe57512c5c147d9322
GBTC441002UR <snowming96@gmail.com>
2018-03-14 13:49:32 -0400

    finished

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -12,14 +12,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Override
-    /*protected void configure(HttpSecurity http) throws Exception{
+    protected void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .and().httpBasic();
-    }*/ //part 1 cannot login in yet
+    }
     protected void configure(AuthenticationManagerBuilder auth)
         throws Exception{
         auth.inMemoryAuthentication().
